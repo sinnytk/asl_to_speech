bfac32721f304ab7b75990dd8d471308cb01ad10
Tarun Kumar <ttarunkkumar99@gmail.com>
2021-01-10 17:30:59 +0500

    Hard coded annotations for testing

---
+++ asl_to_speech/app/static/app/js/camera_app.js
@@ -0,0 +1,34 @@
+function start_mock_translation() {
+  var annotationHtml = document.getElementsByClassName("annotation-section")[0];
+  annotationHtml.style.display = "block";
+  setTimeout(function () {
+    var sample_text = "Testing, testing, testing, mock output";
+    document.getElementById("loader").style.display = "none";
+    let i = 1;
+    for (let el of sample_text) {
+      setTimeout(function () {
+        console.log(el);
+        document.getElementsByClassName("output")[0].innerHTML += el;
+      }, i * 1000);
+      i += 1;
+    }
+  }, 1500);
+}
+$(document).ready(function () {
+  $("#file-upload-btn").click(function () {
+    var constraints = { audio: true, video: { width: 1280, height: 720 } };
+    navigator.mediaDevices
+      .getUserMedia(constraints)
+      .then(function (mediaStream) {
+        var video = document.querySelector("video");
+        video.srcObject = mediaStream;
+        video.onloadedmetadata = function (e) {
+          video.play();
+          start_mock_translation();
+        };
+      })
+      .catch(function (err) {
+        console.log(err.name + ": " + err.message);
+      });
+  });
+});
--- asl_to_speech/app/views.py
+++ asl_to_speech/app/views.py
@@ -1,5 +1,23 @@
+from django.http.response import JsonResponse
 from django.shortcuts import render
 
 
 def index(request):
     return render(request, "app/app.html", {})
+
+
+def image(request):
+    if request.method == "POST":
+        return JsonResponse({"annotation": "A"})
+
+    return render(request, "app/image_app.html", {})
+
+
+def video(request):
+    if request.method == "POST":
+        return JsonResponse({"annotation": "Welcome to ASL To Speech, testing video"})
+    return render(request, "app/video_app.html", {})
+
+
+def camera(request):
+    return render(request, "app/camera_app.html", {})

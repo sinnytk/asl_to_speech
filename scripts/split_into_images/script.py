71a757fd1783211766be47bcbebaabf47d169773
Tarun Kumar <ttarunkkumar99@gmail.com>
2021-07-03 02:29:02 +0500

    Update split script to split all videos in a path

--- scripts/split_into_images/script.py
+++ scripts/split_into_images/script.py
@@ -6,14 +6,16 @@ from tqdm import tqdm
 from pathlib import Path
 
 
+CONSTANT = 0
+DIRNAME = os.path.dirname(__file__)
+
 
 def exit_with_error(msg):
     print(msg)
     exit(1)
 
-
-def split_frames(video_path, dest_path):
-    video = cv2.VideoCapture(str(video_path))
+def split_frames(source_path: Path, dest_path: Path, file):
+    video = cv2.VideoCapture(str(source_path)+'/'+file)
     frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
 
     # image_folder_name = os.path.splitext(video_path)[0].split('/')[-1]
@@ -21,11 +23,13 @@ def split_frames(video_path, dest_path):
 
     success, image = video.read()
     count = 0
+    video_file_name = file.split('.')[0]
     print(f"Writing frames at {dest_path}")
-
+    dest_path = os.path.join(DIRNAME, dest_path)
     pbar = tqdm(total=frames)
-    while success:
-        cv2.imwrite(f"{dest_path}/frame-{count}.jpg", image)
+    while success:  
+        save_file = video_file_name + '_frame_' + str(count) + '.jpg'
+        cv2.imwrite(os.path.join(dest_path, save_file), image)
         success, image = video.read()
         count += 1
         pbar.update(1)
@@ -40,9 +44,9 @@ def main():
             "Script expected ONLY two command line arguments" +
             "\nMake sure you inserted video path")
     # creating video path
-    video_path = Path(sys.argv[1])
+    source_path = Path(sys.argv[1])
     # creating default destination path
-    dest_path = Path(os.path.dirname(video_path) + '/transformed')
+    dest_path = Path(os.path.dirname(source_path) + '/transformed')
 
     if (len(sys.argv) == 3):
         dest_path = Path(sys.argv[2])
@@ -50,8 +54,10 @@ def main():
     if not os.path.exists(dest_path):
         os.makedirs(dest_path)
 
-    if os.path.exists(video_path):
-        split_frames(video_path, dest_path)
+    if os.path.exists(source_path):
+        for file in os.listdir(source_path) :
+            if file.endswith(('.mp4','.webm')):  
+                split_frames(source_path, dest_path, file)
     else:
         exit_with_error("Video doesn't exist")
 

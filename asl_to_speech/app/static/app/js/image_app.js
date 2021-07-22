e1c630ded170a8c15e069ca6abf8f986fdcde2dd
Tarun Kumar <ttarunkkumar99@gmail.com>
2021-07-22 19:53:11 +0500

    Refactor base_app and image_app from scratch

---
+++ asl_to_speech/app/static/app/js/image_app.js
@@ -0,0 +1,68 @@
+let uploaderInputSelector = '.uploader__input'
+let uploaderOutputSelector = '.uploader__output'
+
+function showOutput(output) {
+    const outputBlock = $('p.output');
+    outputBlock.html(output);
+}
+
+function handleNewImage(imageFile) {
+    toggleDisplay(uploaderOutputSelector, true);
+    showOutput("")
+    
+    showUploadedImage(imageFile, `${uploaderOutputSelector} img`, `${uploaderOutputSelector} p`);
+
+    toggleDisplay(annotationSectionSelector, true);
+    toggleLoader(annotationSectionSelector, true);
+
+    annotateImage(imageFile)
+        .then(
+            (res) => {
+                if(res.status === 200) {
+                    res.json().then(
+                        (json) => {
+                            showOutput(json.annotation);
+                        }
+                    )
+                }
+                else {
+                    console.error("Something went wrong");
+                }
+                toggleLoader(annotationSectionSelector, false);
+            },
+            (err) => {
+                console.error(err);
+            }
+        );
+}
+
+$(document).ready(function() {
+    let $form    = $('#file-upload-form')
+    let $inputEl = $('#file') 
+
+    // stop default actions on all drag events
+    $form.on('drag dragstart dragend dragover dragenter dragleave drop', function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+    })
+    // add class to form when dragging over
+    .on('dragover dragenter', function() {
+        $form.addClass('is-dragover');
+    })
+    // remove class from form when dragging out
+    .on('dragleave dragend drop', function() {
+        $form.removeClass('is-dragover');
+    })
+    // handle dropped image
+    .on('drop', function(e) {
+        const droppedImage = e.originalEvent.dataTransfer.files[0];
+        handleNewImage(droppedImage);
+    });
+
+
+    // handle manual image upload
+    $inputEl.on('change', function(e) {
+        const uploadedFile = this.files[0]
+        handleNewImage(uploadedFile);
+    });
+});
---
+++ asl_to_speech/app/static/app/js/lib.js
@@ -0,0 +1,43 @@
+function getCookie(name) {
+    let cookieValue = null;
+
+    if (document.cookie && document.cookie !== "") {
+        const cookies = document.cookie.split(";");
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === name + "=") {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+// get the CSRF token from cookies
+function getCsrfToken() {
+    const csrftoken = getCookie("csrftoken");
+    return csrftoken;
+}
+
+// Add or remove 'hidden' from element classList 
+function toggleDisplay(selector, show) {
+    const $elem = $(selector)
+    if(show) {
+        $elem.removeClass('hidden')
+    }
+    else {
+        $elem.addClass('hidden')
+    }
+}
+
+// Toggle 'loading' class from parent to show or hide loader 
+function toggleLoader(parentSelector, setLoading) {
+    const $elem = $(parentSelector)
+    if(setLoading) {
+        $elem.addClass('loading')
+    } else {
+        $elem.removeClass('loading')
+    }
+}
